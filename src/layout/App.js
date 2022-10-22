5b65ac75eb22a56bb82d5c48bef30010a4e20fcd
ArturoGabrielRamirez <jiamafox@gmail.com>
2022-10-22 12:52:06 -0300

    Add fix for texting live

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,16 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Footer from './Footer'
 import Header from './Header'
 import Main from './Main'
 
+const queryClient = new QueryClient()
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <Header/>
-            <Main/>
-            <Footer/>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Header />
+                <Main />
+                <Footer />
+            </BrowserRouter>
+        </QueryClientProvider>
     )
 }
 
--- src/pages/MessagesPrivate.js
+++ src/pages/MessagesPrivate.js
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useUsersStore from '../api/usersStore';
@@ -6,11 +7,12 @@ import { getMessages, sendMessage } from '../utils';
 const MessagesPrivate = () => {
 
     const { id } = useParams();
-    const [messages, setMessages] = useState({messages:[]});
+    const [messages, setMessages] = useState({ messages: [] });
     const [currentMessage, setCurrentMessage] = useState('');
     const { uid } = useUsersStore(({ current }) => current);
+    const { data } = useQuery(['messages'], getMessages.bind(null, id, uid), { refetchInterval: 500 });
 
-    console.log(messages)
+
 
     useEffect(() => {
         handleMessages();
@@ -35,18 +37,18 @@ const MessagesPrivate = () => {
             unread: true
         }
         sendMessage(id, message)
-        .then(()=>{
-            setCurrentMessage('');
-            handleMessages();
-        })
+            .then(() => {
+                setCurrentMessage('');
+                handleMessages();
+            })
     }
 
-    const { messages : messagesList } = messages
+    const { messages: messagesList } = messages
 
     return (
         <div className='messages-private'>
             <div className="messages-list">
-                {messagesList && messagesList.map((message, i) => (
+                {data && data.messages.map((message, i) => (
                     <div key={i} className={`message ${message.sender === uid ? 'message--sent' : 'message--received'}`}>
                         <div className={`message__content ${message.sender == uid ? "message_right" : "message_left"}`}>{message.message}</div>
                     </div>
